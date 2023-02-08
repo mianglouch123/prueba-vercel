ecf2b8bd8f58bcd1e627b15d7a84ae3930033aca
mianglouch123 <miangpermer@gmail.com>
2023-02-08 17:22:11 -0500

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 1000;
 
 app.get('/', (req, res) => {
   res.send('Bienvenido!')
@@ -11,7 +11,7 @@ app.get('/calculate',(req,res)=>{
   const firstValue = req.query.f;
   const secondValue = req.query.s;
 
-  res.send(`values` + firstValue + "" + secondValue)
+  res.send(`values` + firstValue + "" + secondValue);
 })
 
 app.listen(port, () => {
