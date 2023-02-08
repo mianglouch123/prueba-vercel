b1d896fc0f01f4e0eb9cf52073358d1eeb0a54cb
mianglouch123 <miangpermer@gmail.com>
2023-02-08 17:17:56 -0500

    prueba endpoint

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Bienvenido!')
 })
 
+
+app.get('/calculate',(req,res)=>{
+  const firstValue = req.query.f;
+  const secondValue = req.query.s;
+
+  res.send(`values` + firstValue + "" + secondValue)
+})
+
 app.listen(port, () => {
   console.log(`Bienvenido port ${port}`)
 })
